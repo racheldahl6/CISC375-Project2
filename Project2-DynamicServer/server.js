3610f208596d95e14b343299f79e01929080afd6
eggsncheese <50417771+eggsncheese@users.noreply.github.com>
2019-10-10 15:36:14 -0500

    Update server.js

--- Project2-DynamicServer/server.js
+++ Project2-DynamicServer/server.js
@@ -2,6 +2,7 @@
 var fs = require('fs')
 var path = require('path')
 
+
 // NPM modules
 var express = require('express')
 var sqlite3 = require('sqlite3')
@@ -21,23 +22,32 @@ var db = new sqlite3.Database(db_filename, sqlite3.OPEN_READONLY, (err) => {
     }
     else {
         console.log('Now connected to ' + db_filename);
-        TestSql();
+        //TestSql();
     }
 });
 
-function TestSql() {
+function CoalTestSql() {
+	return new Promise( function(res,rej) {
+		coalSum = 0;
 
-	let sql = 'SELECT * FROM consumption WHERE year = ?';
+		let sql = 'SELECT coal FROM consumption WHERE year = ?';
 
-	db.all(sql, ['2017'], (err, rows) => {
+		db.all(sql, ['2017'], (err, rows) => {
 
-		if(err){
-			throw err;
+			if(err){
+				rej(err);
 
-		}
-		rows.forEach((row) => {
-			console.log(row);
+			}
+			rows.forEach((row) => {
+				coalSum = coalSum + row.coal;
+
+			});
+
+			console.log(coalSum + "one");
+			res(coalSum);
+
 		});
+	
 	});
 }
 
@@ -46,15 +56,26 @@ function TestSql() {
 app.use(express.static(public_dir));
 
 
-// GET request handler for '/'
+// GET request handler for '/' HOME PAGE
 app.get('/', (req, res) => {
-    ReadFile(path.join(template_dir, 'index.html')).then((template) => {
-        let response = template;
-        // modify `response` here
-        template = template.toString();
-        template = template.replace('!COALCOUNT!', '10');
+	
+    //var coal = coalsum.toString();
 
-        WriteHtml(res, response);
+    ReadFile(path.join(template_dir, 'index.html')).then((template) => {
+        var coal;
+    	CoalTestSql().then(function(coalSum) {
+    		console.log(coalSum);
+    		template = template.toString();
+	        template = template.replace('!COALCOUNT!', coalSum);
+	        let response = template;
+
+	        WriteHtml(res, response);
+	    });
+
+        //console.log(coal + " COAL SUM ");
+
+        // modify `response` here
+        
     }).catch((err) => {
         Write404Error(res);
     });
@@ -63,8 +84,13 @@ app.get('/', (req, res) => {
 // GET request handler for '/year/*'
 app.get('/year/:selected_year', (req, res) => {
     ReadFile(path.join(template_dir, 'year.html')).then((template) => {
-        let response = template;
+
         // modify `response` here
+        template = template.toString();
+        //console.log(req.params.selected_year);
+        template = template.replace('!YEAR!', req.params.selected_year);
+
+        let response = template;
         WriteHtml(res, response);
     }).catch((err) => {
         Write404Error(res);
@@ -74,9 +100,14 @@ app.get('/year/:selected_year', (req, res) => {
 // GET request handler for '/state/*'
 app.get('/state/:selected_state', (req, res) => {
     ReadFile(path.join(template_dir, 'state.html')).then((template) => {
-        let response = template;
+        
         // modify `response` here
+        template = template.toString();
+        template = template.replace('!STATE!', req.params.selected_state);
+
+        let response = template;
         WriteHtml(res, response);
+
     }).catch((err) => {
         Write404Error(res);
     });
@@ -85,8 +116,12 @@ app.get('/state/:selected_state', (req, res) => {
 // GET request handler for '/energy-type/*'
 app.get('/energy-type/:selected_energy_type', (req, res) => {
     ReadFile(path.join(template_dir, 'energy.html')).then((template) => {
-        let response = template;
+        
         // modify `response` here
+        template = template.toString();
+        template = template.replace('!ENERGY!', req.params.selected_energy_type);
+
+        let response = template;
         WriteHtml(res, response);
     }).catch((err) => {
         Write404Error(res);
